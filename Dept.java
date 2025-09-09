0e92fe7726ef3d340b3826369ca7e6e720afc129
Prashant3680 <patilprashant3680@gmail.com>
2025-09-09 23:46:08 +0530

    Add files via upload

---
+++ Dept.java
@@ -0,0 +1,16 @@
+public class Dept
+
+{
+	String deptNo;
+	String dname;
+	String loc;
+	
+	Dept(){}
+	
+	Dept(String deptNo,String dname,String loc)
+	{
+		this.deptNo=deptNo;
+		this.dname=dname;
+		this.loc=loc;
+	}
+}
---
+++ Emp.java
@@ -0,0 +1,26 @@
+public class Emp 
+{
+	String empNo;
+	String ename;
+	String job;
+	String mgr;
+	String hiredate;
+	String sal;
+	String comm;
+	String deptNo;
+	
+	public Emp(){}
+	
+	public Emp(String empNo,String ename,String job,String mgr,String hireDate,String sal,String comm,String deptNo)
+	{
+		this.empNo=empNo;
+		this.ename=ename;
+		this.job=job;
+		this.mgr=mgr;
+		this.hiredate=hireDate;
+		this.sal=sal;
+		this.comm=comm;
+		this.deptNo=deptNo;
+	}
+	
+}
